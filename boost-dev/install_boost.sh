d7539eea65fc896e3f44fb99c50c059f3a46e4cf
Roman Gershman <romange@gmail.com>
2018-09-07 22:20:08 +0300

    .

--- boost-dev/install_boost.sh
+++ boost-dev/install_boost.sh
@@ -7,7 +7,7 @@ BOOST_NAME=boost_${BOOST_VERSION}
 wget -nv "https://dl.bintray.com/boostorg/release/$(echo ${BOOST_VERSION} | tr '_' '.')/source/${BOOST_NAME}.tar.bz2"
 tar -xjf ${BOOST_NAME}.tar.bz2 && rm ${BOOST_NAME}.tar.bz2 && cd ${BOOST_NAME}
 
-./bootstrap.sh --prefix=/usr/local --without-libraries=graph_parallel,graph,wave,test,mpi,python
+./bootstrap.sh --prefix=/opt/boost --without-libraries=graph_parallel,graph,wave,test,mpi,python
 ./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
          --without-test --without-python --without-mpi --without-graph --without-regex --without-wave --without-random --without-contract \
          --without-locale -j4
