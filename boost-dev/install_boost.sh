f4ff6f0d72fcf871ad8c3f6b1cc16978abad0a2d
Roman Gershman <romange@gmail.com>
2018-09-07 16:19:54 +0300

    Optimize booster image

---
+++ boost-dev/install_boost.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BOOST_NAME=$1
+
+wget -nv "https://dl.bintray.com/boostorg/release/$(echo ${BOOST_VERSION} | tr '_' '.')/source/${BOOST_NAME}.tar.bz2"
+tar -xjf ${BOOST_NAME}.tar.bz2 && rm ${BOOST_NAME}.tar.bz2 && cd ${BOOST_NAME}
+
+./bootstrap.sh --prefix=/usr/local --without-libraries=graph_parallel,graph,wave,test,mpi,python
+./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
+         --without-test --without-python --without-mpi --without-graph --without-regex --without-wave --without-random --without-contract \
+         --without-locale -j4
+./b2 -d0 install
+cd - && rm -rf ${BOOST_NAME}
--- cpp-dev18.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM romange/ubuntu-gcc
-
-COPY --from=romange/boost-builder /opt/boost /usr/local
-RUN apt-get install -y  --no-install-recommends libbz2-dev
