73791ac5e8b3618fae2084ba73d01a377fc34fb5
Roman Gershman <romange@gmail.com>
2018-09-08 16:08:18 +0300

    .

--- boost-prod/install.sh
+++ boost-prod/install.sh
@@ -3,14 +3,21 @@ set -o xtrace
 
 DIST=$1
 
-alias setup='apt-get install -y --no-install-recommends'
+setup() {
+  apt-get install -y --no-install-recommends $@
+}
+
+
+if [ "${DIST}" = "16" ]; then
+    echo 'deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main' >>  /etc/apt/sources.list.d/toolchain.list
+    echo 'deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main' >>  /etc/apt/sources.list.d/toolchain.list
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1E9377A2BA9EF27F
+fi
 
 apt-get update && setup libunwind8 binutils htop
 
 if [ "${DIST}" = "18" ]; then
   setup libevent-2.1-6 libevent-pthreads-2.1-6
 else
-  setup  libevent-2.0-5 libevent-pthreads-2.0-5 software-properties-common && add-apt-repository ppa:ubuntu-toolchain-r/test
-  apt-get update
-  setup gcc-7-base
+  setup  libevent-2.0-5 libevent-pthreads-2.0-5 gcc-7-base
 fi
