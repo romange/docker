4b6d7d14fdc7ea3ec5512e1f32b7f2f365d21d46
Roman Gershman <romange@gmail.com>
2020-04-18 20:59:38 +0300

    Add 20.04

--- ugcc/install.sh
+++ ugcc/install.sh
@@ -15,13 +15,15 @@ if [ "${DIST}" = "16" ]; then
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1E9377A2BA9EF27F
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 setup libunwind8 binutils htop bzip2 wget ca-certificates g++-${GCC_VER} libunwind-dev  \
       ninja-build ccache libbz2-dev git make autoconf curl unzip automake \
-      zlib1g-dev python3-setuptools zip libtool libssl-dev libxml2-dev
+      zlib1g-dev python3-setuptools zip libtool libssl-dev libxml2-dev cmake
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VER} 60 \
                     --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VER}
 
-wget https://github.com/Kitware/CMake/releases/download/v3.12.2/cmake-3.12.2-Linux-x86_64.sh
-sh cmake-3.12.2-Linux-x86_64.sh --skip-license --prefix=/usr/local
+ln -fs /usr/share/zoneinfo/Greenwich /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
