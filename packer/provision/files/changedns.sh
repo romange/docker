823674d6e39145fab1d454858a9911e3fbf7686b
Roman Gershman <romange@gmail.com>
2020-11-17 13:52:24 +0200

    Update changedns.sh

--- packer/provision/files/changedns.sh
+++ packer/provision/files/changedns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Extract information about the Instance
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
 MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
